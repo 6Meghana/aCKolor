191735cb488d0de8ac95f5ecbdaebe276d9d6da9
6meghana <meghana.n175@gmail.com>
2017-06-19 11:46:40 +0530

    now

--- src/script/ckolorpicker-factory.js
+++ src/script/ckolorpicker-factory.js
@@ -78,11 +78,9 @@
 					}
 					localStorage.aCKolorPreviousColors = angular.toJson(self.previousColors);
 				}
-<<<<<<< HEAD
+
 			//	self.toggleCKoloring();//to close the color picker
-=======
-			/*self.toggleCKoloring(); */
->>>>>>> 916c07be169adcf7e953e9d5826437ceecd64544
+
 			},
 
 			previousColorClick: function(color){
